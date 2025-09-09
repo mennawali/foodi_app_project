ed01b932d6bb031bc2bd831b2740377354a6aac6
ShahdAhmed134 <enshahd134@gmail.com>
2025-09-09 11:48:40 +0300

    Api recipes.

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,14 +1,13 @@
 
 import 'package:flutter/material.dart';
+import 'package:foodi_app_project/home_page.dart';
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
      return MaterialApp(
        debugShowCheckedModeBanner: false,
-       home: Scaffold(
-         backgroundColor: Colors.white,
-       ),
+       home: HomePage()
      );
   }
 
